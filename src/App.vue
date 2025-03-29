f31b65176cb47ec7b6ce50fa2fceff8d9629b248
Cesar Figueroa <cesarfig117@gmail.com>
2025-03-29 14:25:13 -0600

    Tarea terminada

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
     const store = useLayoutStore ()
     
     const mainStyles = {
-      marginLeft: ${store.sidebarWidth}px -30px,
+      marginLeft: `${store.sidebarWidth}px -30px`,
       transition: 'margin 0.3s cubic-bezier(0.25, 0.8, 0.5, 1)'
     }
 
